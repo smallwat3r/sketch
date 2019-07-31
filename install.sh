55f27710c0636a602092b4c5adf85ab22874180f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2019-07-31 04:33:15 +0100

    add config

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# File              : install.sh
-# Author            : Matthieu Petiteau <mpetiteau.pro@gmail.com>
-# Date              : Wed 31 Jul 04:05:08 2019
-# Last Modified Date: Wed 31 Jul 04:05:08 2019
-# Last Modified By  : Matthieu Petiteau <mpetiteau.pro@gmail.com>
-
-cp src/sketch .sketchconfig ~
-chmod +x ~/sketch
